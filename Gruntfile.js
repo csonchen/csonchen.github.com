ecdfe78d137747f28cf4eddc48c355a6615c82f0
csonchen <cson_chensheng@163.com>
2016-12-07 22:46:47 +0800

    '可以进行sass并且通过grunt来编译为css了'

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,22 @@
 /**
  * Created by 胜 on 2016/12/7.
  */
+'use strict';
+
+module.exports = function (grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    sass: {
+      dist: {
+        files: {
+          'app/css/screen.css': 'app/sass/screen.sass'
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-sass');
+
+  grunt.registerTask('default', ['sass']);
+};
