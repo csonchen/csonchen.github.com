589ec51e2abde727439217ee4ea1c8d251d4c4a0
csonchen <cson_chensheng@163.com>
2016-12-10 18:37:49 +0800

    '打包压缩css文件'

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,16 +7,31 @@ module.exports = function (grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
+    // 将sass文件编译为css
     sass: {
       dist: {
         files: {
-          'app/css/screen.css': 'app/sass/screen.sass'
+          'css/screen.css': 'sass/screen.sass'
         }
+      }
+    },
+
+    // 压缩css文件
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'css',
+          src: '*.css',
+          dest: 'css',
+          ext: '.min.css'
+        }]
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
   grunt.registerTask('default', ['sass']);
 };
